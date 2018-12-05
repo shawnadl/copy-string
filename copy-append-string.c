4ff1a2cbd53447d0a5e2410f4c77a279ade18e3b
shawnadl <45616017+shawnadl@users.noreply.github.com>
2018-12-05 14:36:08 +0900

    Update copy-append-string.c

--- copy-append-string.c
+++ copy-append-string.c
@@ -1,8 +1,10 @@
-//- copy and append string
-//- practice-1 2018/05/12
+/*************************
+ *Copy and append string
+ *Practice-1 2018/05/12
+ ************************/
 
 //This function uses pointer arithmetic to copy string 2 to string 1
-void copy_string(char* s1, char* s2)
+void copy_string(char* s1, const char* s2)
 { 
   for( ; (*s1 = *s2) != '\0' ; s1++, s2++){
     ;//do nothing in body
@@ -10,7 +12,7 @@ void copy_string(char* s1, char* s2)
 }
 
 //This function uses pointer arithmetic to append string 2 to string 1
-void append_string(char* s1, char* s2)
+void append_string(char* s1, const char* s2)
 {
   for( ; *s1 != '\0' ; s1++){
     ;//do nothing in body
@@ -19,3 +21,7 @@ void append_string(char* s1, char* s2)
     ;//do nothing in body
    }
 }
+/*The first argument to both copy and append must be an array large enough to hold the
+string in the second argument. Otherwise, an error may occur when an attempt is made to
+write into a memory location that’s not part of the array. Also, the second parameter of
+each function is declared as const char * (a constant string)*/
